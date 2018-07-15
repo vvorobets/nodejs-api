43dc9f6575d0cef98e5075958d17f49dcecee7c4
vvorobets <vvorobets@gmail.com>
2018-07-15 09:39:19 +0300

    Fixed array query

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,21 +1,26 @@
 const router = require("express").Router();
 // const getReceivers = require("../../services/user");
 
-let receivers = [];
 const Message = require('../../models/message.js');
 const User = require('../../models/user.js');
 
 router.get("/:id", (req, res, next) => {
   // receivers.getReceivers(Number(req.params.id), (err, data) => {
   Message.find( {senderId: req.params.id}, 'receiverId -_id', (err, messages) => {
+    let receivers = [];
 		if (!err) {
       for(let item of messages) {
         if(receivers.indexOf(item.receiverId) < 0) {
           receivers.push(item.receiverId);
         }; 
       };
-      User.find({id:{$in:[receivers]}}, 'name', users => {
-        res.json(users);
+      User.find({id:{$in: receivers}}, 'name -_id', (err, users) => {
+        if(!err) {
+          res.json(users);
+        } else {
+          res.status(400);
+          res.end();
+        }
       });
   } else {
       res.status(400);
